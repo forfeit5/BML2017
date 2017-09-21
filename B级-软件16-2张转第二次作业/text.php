eae84497cb0741e0b7f5d5a2dd2f8742ffbbe8ef
forfeit5 <32080771+forfeit5@users.noreply.github.com>
2017-09-22 07:52:30 +0800

    B级软件16-2张转

---
+++ B级-软件16-2张转第二次作业/text.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<meta charset="UTF-8">
+<style type="text/css">
+a
+{
+	color: black;
+	position:relative;
+    left:300px;
+}
+input{
+	position:relative;
+    left:650px;
+}
+</style>
+</head>
+<body background="背景图片1.jpg">
+
+<div>
+<form action="index.html" method="post">
+<br><br><br><br>
+<a href="新闻5.txt">【喜迎十九大】强军兴军，向世界一流军队迈进</a><br><br>
+<a href="新闻3.txt">墨西哥强震致5名台湾同胞受困：其中2人不幸罹难</a><br><br>
+<a href="新闻1.txt">《和陌生人说话》登陆学院奖，这个创意挑战很不一样！</a><br><br>
+<a href="新闻2.txt">放心去善良，“新闻+公益”模式有什么不一样？</a><br><br>
+<input type="submit" value="返回">
+</form>	
+</div>
+
+</body>
+</html>
+
